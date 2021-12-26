b5fcf802be8e280b30d68d6908f13522d3493bd7
codingwithtashi <khakontas33@gmail.com>
2021-12-26 14:23:55 +0530

    Part 3- Database set up

---
+++ lib/database/database.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+part 'database.g.dart';
+
+class Note extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  TextColumn get description => text().named('description')();
+  IntColumn get priority => integer().nullable()();
+  IntColumn get color => integer().nullable()();
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+
+@DriftDatabase(tables: [Note])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/database/database.dart';
 import 'package:note_keeper/screen/note_list_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,34 +13,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        textTheme: const TextTheme(
-          headline5: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 24),
-          bodyText2: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 20),
-          bodyText1: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontSize: 18),
-          subtitle2: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontSize: 14),
+    return Provider(
+      create: (context) => AppDatabase(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          textTheme: const TextTheme(
+            headline5: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 24),
+            bodyText2: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 20),
+            bodyText1: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 18),
+            subtitle2: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 14),
+          ),
         ),
+        home: const NoteListPage(),
       ),
-      home: const NoteListPage(),
     );
   }
 }
