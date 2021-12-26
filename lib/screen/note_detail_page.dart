fd0163859c385049edd6b81214703d3dd978fcb4
codingwithtashi <khakontas33@gmail.com>
2021-12-26 14:11:15 +0530

    Initial commit

---
+++ lib/screen/note_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteDetailPage extends StatefulWidget {
+  const NoteDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _NoteDetailPageState createState() => _NoteDetailPageState();
+}
+
+class _NoteDetailPageState extends State<NoteDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screen/note_list_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NoteListPage extends StatefulWidget {
+  const NoteListPage({Key? key}) : super(key: key);
+
+  @override
+  _NoteListPageState createState() => _NoteListPageState();
+}
+
+class _NoteListPageState extends State<NoteListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
