7c5fc221ec09edebc49e9a65693d530f8f71befd
codingwithtashi <khakontas33@gmail.com>
2021-12-26 14:36:23 +0530

    Part 4- Fetch Note

--- lib/screen/note_list_page.dart
+++ lib/screen/note_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper/database/database.dart';
+import 'package:provider/provider.dart';
 
 class NoteListPage extends StatefulWidget {
   const NoteListPage({Key? key}) : super(key: key);
@@ -8,15 +10,63 @@ class NoteListPage extends StatefulWidget {
 }
 
 class _NoteListPageState extends State<NoteListPage> {
+  late AppDatabase database;
   @override
   Widget build(BuildContext context) {
+    database = Provider.of<AppDatabase>(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World!',
-          style: Theme.of(context).textTheme.headline5,
+      body: FutureBuilder<List<NoteData>>(
+        future: _getNoteFromDatabase(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<NoteData>? noteList = snapshot.data;
+            if (noteList != null) {
+              if (noteList.isEmpty) {
+                return Center(
+                  child: Text(
+                    'No Notes Found, Click on add button to add new note',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                );
+              } else {
+                return noteListUI(noteList);
+              }
+            }
+          } else if (snapshot.hasError) {
+            return Center(
+                child: Text(
+              snapshot.error.toString(),
+              style: Theme.of(context).textTheme.bodyText2,
+            ));
+          }
+          return Center(
+            child: Text(
+              'Click on add button to add new note',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: CircleBorder(
+          side: BorderSide(color: Colors.black, width: 2),
+        ),
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
         ),
       ),
     );
+  }
+
+  Future<List<NoteData>> _getNoteFromDatabase() async {
+    return await database.getNoteList();
+  }
+
+  Widget noteListUI(List<NoteData> noteList) {
+    return Container();
   }
 }
